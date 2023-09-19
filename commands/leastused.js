08be04ed8055c8ea0e0b405d0576d1b52b7943c7
oshhyy <oshfour@gmail.com>
2023-09-19 00:49:19 -0400

    deprecated 7tv usage commands :(

--- commands/leastused.js
+++ commands/leastused.js
@@ -6,73 +6,71 @@ module.exports = {
     description: `leastused <7tv emote> | https://osh-1.gitbook.io/osh_______/7tv/leastused`,
     execute: async context => {
         try {
+            return{text:`Because of the emote-tracking API going down, this command has been deprecated FeelsBadMan`, reply:true}
             // command code
-            let user = context.message.args[0] ?? context.channel.login;
-            user = user.replace('@','');
-            user = user.replace('#','');
-            let message = `least used 7tv emotes in #${user} `
+            // let user = context.message.args[0] ?? context.channel.login;
+            // user = user.replace('@','');
+            // user = user.replace('#','');
+            // let message = `least used 7tv emotes in #${user} `
 
-            let idData 
-            try{
-                idData = await got(`https://api.ivr.fi/v2/twitch/user?login=${user}`).json();
-            } catch(err) {
-                return {
-                    text:`Error: The provided username is invalid!`, reply:true
-                }
-            }
+            // let idData 
+            // try{
+            //     idData = await got(`https://api.ivr.fi/v2/twitch/user?login=${user}`).json();
+            // } catch(err) {
+            //     return {
+            //         text:`Error: The provided username is invalid!`, reply:true
+            //     }
+            // }
             
 
-            let data;
-            let dataFromKattahsAPI;
-            let enabledEmotes;
-            let filteredEmotes;
+            // let data;
+            // let dataFromKattahsAPI;
+            // let enabledEmotes;
+            // let filteredEmotes;
 
 
-            if (context.message.args[0] == '#global') {
-                try {
-                    data = await got(`https://7tv.io/v3/emote-sets/62cdd34e72a832540de95857`).json()
-                } catch(err) {
-                    return {
-                        text:`Error: No 7tv emotes found for this channel FeelsDankMan`, reply:true
-                    }
-                }
-                dataFromKattahsAPI = await got(`https://api.kattah.me/global?limit=23894792834`).json()
+            // if (context.message.args[0] == '#global') {
+            //     try {
+            //         data = await got(`https://7tv.io/v3/emote-sets/62cdd34e72a832540de95857`).json()
+            //     } catch(err) {
+            //         return {
+            //             text:`Error: No 7tv emotes found for this channel FeelsDankMan`, reply:true
+            //         }
+            //     }
+            //     dataFromKattahsAPI = await got(`https://api.kattah.me/global?limit=23894792834`).json()
 
-                enabledEmotes = data.emotes.map(emote => emote.id)
-                filteredEmotes = dataFromKattahsAPI.emotes.filter(emote => enabledEmotes.includes(emote.emote_id))
-                for (let i = 1; i <= 5 ; i++) {
-                    message = message.concat(` • ${filteredEmotes[filteredEmotes.length - i].emote} (${filteredEmotes[filteredEmotes.length - i].total_count.toLocaleString()})`)
-                }
+            //     enabledEmotes = data.emotes.map(emote => emote.id)
+            //     filteredEmotes = dataFromKattahsAPI.emotes.filter(emote => enabledEmotes.includes(emote.emote_id))
+            //     for (let i = 1; i <= 5 ; i++) {
+            //         message = message.concat(` • ${filteredEmotes[filteredEmotes.length - i].emote} (${filteredEmotes[filteredEmotes.length - i].total_count.toLocaleString()})`)
+            //     }
 
-            } else {
-                try {
-                    data = await got(`https://7tv.io/v3/users/twitch/${idData[0].id}`).json()
-                } catch(err) {
-                    return {
-                        text:`Error: No 7tv emotes found for this channel FeelsDankMan`, reply:true
-                    }
-                }
-                try {
-                    dataFromKattahsAPI = await got(`https://api.kattah.me/c/${user}?limit=23894792834`).json()
-                } catch(err) {
-                    return {
-                        text:`Error: This channel's 7tv emote usage is not tracked! FeelsDankMan`, reply:true
-                    }
-                }
+            // } else {
+            //     try {
+            //         data = await got(`https://7tv.io/v3/users/twitch/${idData[0].id}`).json()
+            //     } catch(err) {
+            //         return {
+            //             text:`Error: No 7tv emotes found for this channel FeelsDankMan`, reply:true
+            //         }
+            //     }
+            //     try {
+            //         dataFromKattahsAPI = await got(`https://api.kattah.me/c/${user}?limit=23894792834`).json()
+            //     } catch(err) {
+            //         return {
+            //             text:`Error: This channel's 7tv emote usage is not tracked! FeelsDankMan`, reply:true
+            //         }
+            //     }
 
-                enabledEmotes = data.emote_set.emotes.map(emote => emote.id)
-                filteredEmotes = dataFromKattahsAPI.emotes.filter(emote => enabledEmotes.includes(emote.emote_id))
-                for (let i = 1; i <= 5 ; i++) {
-                    message = message.concat(` • ${filteredEmotes[filteredEmotes.length - i].emote} (${filteredEmotes[filteredEmotes.length - i].count.toLocaleString()})`)
-                }
-            }
-            
+            //     enabledEmotes = data.emote_set.emotes.map(emote => emote.id)
+            //     filteredEmotes = dataFromKattahsAPI.emotes.filter(emote => enabledEmotes.includes(emote.emote_id))
+            //     for (let i = 1; i <= 5 ; i++) {
+            //         message = message.concat(` • ${filteredEmotes[filteredEmotes.length - i].emote} (${filteredEmotes[filteredEmotes.length - i].count.toLocaleString()})`)
+            //     }
+            // }
 
-            
-
-            return{
-                text:message, reply:true,
-            }
+            // return{
+            //     text:message, reply:true,
+            // }
 
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
--- commands/mostused.js
+++ commands/mostused.js
@@ -6,62 +6,63 @@ module.exports = {
     description: `mostused <7tv emote> https://osh-1.gitbook.io/osh_______/7tv/mostused`,
     execute: async context => {
         try {
+            return{text:`Because of the emote-tracking API going down, this command has been deprecated FeelsBadMan`, reply:true}
             // command code
-            let message = '';
-            let user = '';
-            console.log(user)
-            user = user.replace('@','');
-            user = user.replace('#','');
-            if (!context.message.args[0]) {
-                user = `c/${context.channel.login}`
-            }
-            else {
-                user = `c/${context.message.args[0]}`
-                if (context.message.args[0].includes('/')) {
-                    return {
-                        text:'Invalid Username! FeelsDankMan', reply:true
-                    };
-                }
-            }
+            // let message = '';
+            // let user = '';
+            // console.log(user)
+            // user = user.replace('@','');
+            // user = user.replace('#','');
+            // if (!context.message.args[0]) {
+            //     user = `c/${context.channel.login}`
+            // }
+            // else {
+            //     user = `c/${context.message.args[0]}`
+            //     if (context.message.args[0].includes('/')) {
+            //         return {
+            //             text:'Invalid Username! FeelsDankMan', reply:true
+            //         };
+            //     }
+            // }
 
-            if (context.message.args[0] == '#global') {user = 'global'}
-            if (context.message.args[0] == '#top') {user = 'top'}
+            // if (context.message.args[0] == '#global') {user = 'global'}
+            // if (context.message.args[0] == '#top') {user = 'top'}
 
-            let data;
-            try {
-                data = await got(`https://api.kattah.me/${user}`).json()
-            } catch(err) {
-                return {
-                    text:`Either no 7tv emotes are enabled in this channel, or their usage statistics haven't been tracked yet FeelsDankMan`, reply:true
-                }
-            }
+            // let data;
+            // try {
+            //     data = await got(`https://api.kattah.me/${user}`).json()
+            // } catch(err) {
+            //     return {
+            //         text:`Either no 7tv emotes are enabled in this channel, or their usage statistics haven't been tracked yet FeelsDankMan`, reply:true
+            //     }
+            // }
 
-            if (!data) {
-                return {
-                    text:'Invalid Username! FeelsDankMan', reply:true
-                };
-            } if (data.success == false) {
-                return {
-                    text:'No emotes found for this channel.', reply:true
-                };
-            }
+            // if (!data) {
+            //     return {
+            //         text:'Invalid Username! FeelsDankMan', reply:true
+            //     };
+            // } if (data.success == false) {
+            //     return {
+            //         text:'No emotes found for this channel.', reply:true
+            //     };
+            // }
 
-            if (user == "global" || user == "top") {
-                message = `top 5 7tv emotes (${user})`
-                for (let i = 0; i < 5 ; i++) {
-                    message = message.concat(` • ${data.emotes[i].emote} (${data.emotes[i].total_count.toLocaleString()})`)
-                }
-            } else {
-                message = `top 5 7tv emotes in #${data.user.twitch_username}`
-                for (let i = 0; i < 5 ; i++) {
-                    message = message.concat(` • ${data.emotes[i].emote} (${data.emotes[i].count.toLocaleString()})`)
-                }
-            } 
+            // if (user == "global" || user == "top") {
+            //     message = `top 5 7tv emotes (${user})`
+            //     for (let i = 0; i < 5 ; i++) {
+            //         message = message.concat(` • ${data.emotes[i].emote} (${data.emotes[i].total_count.toLocaleString()})`)
+            //     }
+            // } else {
+            //     message = `top 5 7tv emotes in #${data.user.twitch_username}`
+            //     for (let i = 0; i < 5 ; i++) {
+            //         message = message.concat(` • ${data.emotes[i].emote} (${data.emotes[i].count.toLocaleString()})`)
+            //     }
+            // } 
 
-            return {
-                text: message,
-                reply: true,
-            };
+            // return {
+            //     text: message,
+            //     reply: true,
+            // };
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
             console.log(err);
