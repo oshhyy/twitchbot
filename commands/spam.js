5bc6ee1f754ba35f4f31a513196f3f2ad080d586
oshhyy <oshfour@gmail.com>
2023-10-04 12:43:21 -0400

    removed vips from spam

--- commands/spam.js
+++ commands/spam.js
@@ -13,9 +13,9 @@ module.exports = {
             if (!context.badges.hasModerator && !context.badges.hasBroadcaster && !context.badges.hasVIP && context.user.id != "489223884") {
                 return {};
             }
-            if(!context.botBadges.badges.hasModerator && !context.botBadges.badges.hasVIP && !context.botBadges.badges.hasBroadcaster && !context.message.params.channel) {
+            if(!context.botBadges.badges.hasModerator && !context.botBadges.badges.hasBroadcaster && !context.message.params.channel) {
                 return {
-                    text: `I can't perform this command because I am not moderator or vip! forsenNOIDONTTHINKSO`, reply: true
+                    text: `I can't perform this command because I am not moderator! forsenNOIDONTTHINKSO`, reply: true
                 };
             }
 
