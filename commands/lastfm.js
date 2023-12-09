213f31499a26ff98404e9ae45a83acb9aec86322
oshhyy <oshfour@gmail.com>
2023-12-09 15:19:26 -0500

    123

--- commands/lastfm.js
+++ commands/lastfm.js
@@ -4,7 +4,7 @@ const config = require("../config.json");
 module.exports = {
     name: "lastfm",
     cooldown: 3000,
-    aliases: ["currentsong"],
+    aliases: ["lastfmstats"],
     description: `lastfm [lastfm-username] - shows stats for last.fm`,
     execute: async context => {
         try {
