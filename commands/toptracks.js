761ea2e9980b35a981daa06643245f3ed633d1ee
oshhyy <oshfour@gmail.com>
2023-12-09 16:25:04 -0500

    SDHFBGUIDHBU

--- commands/toptracks.js
+++ commands/toptracks.js
@@ -14,13 +14,14 @@ module.exports = {
             if(!lastfmName){
                 return{text:`No last.fm username provided! To link your account, do '+link <username>'`, reply:true}
             }
-            const data = await got(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${lastfmName}&api_key=${config.lastfmKey}&format=json&limit=1&extended=1`, {throwHttpErrors:false}).json()
+            const data = await got(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${lastfmName}&api_key=${config.lastfmKey}&format=json`, {throwHttpErrors:false}).json()
             if(data.message) {
                 return{text:data.message, reply:true}
             }
 
             let message = `top tracks for ${data.toptracks[`@attr`].user}`
             let i;
+            console.log(data.toptracks)
             for(i = 0; i < 5; i++) {
                 message = message.concat(` • ${data.toptracks.track[i].artist.name} - ${data.toptracks.track[i].name} (${data.toptracks.track[i].playcount.toLocaleString()})`)
             }
