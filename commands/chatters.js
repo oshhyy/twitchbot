24680738ad732707a50b7d582314b31cd13f3ba8
oshhyy <oshfour@gmail.com>
2023-12-30 10:31:52 -0500

    fix spewllign error

--- commands/chatters.js
+++ commands/chatters.js
@@ -29,7 +29,7 @@ module.exports = {
                 };
             }
 
-            return{text:`there are ${data.count} in ${channel}'s chat chatters`}
+            return{text:`there are ${data.count} in ${channel}'s chat.`}
 
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
