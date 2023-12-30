ed33eee40f4c2e7c5c44fa77645f2fc677f66cdc
oshhyy <oshfour@gmail.com>
2023-12-30 10:31:08 -0500

    chatters command

---
+++ commands/chatters.js
@@ -0,0 +1,40 @@
+const got = require("got");
+
+module.exports = {
+    name: "chatters",
+    cooldown: 5000,
+    aliases: ['chattercount'],
+    description: `chatters [channel] | shows the number of chatters in specified/current channel`,
+    execute: async context => {
+        try {
+            // command code
+            let channel = context.message.args[0] ?? context.channel.login;
+            channel = channel.replace('@','');
+            if (!context.message.args[0]) {}
+            else { 
+                if (context.message.args[0].includes('/')) {
+                    if (context.message.args[0].includes('/')) {
+                        return {
+                            text:'Invalid Username! oshDank', reply:true
+                        };
+                    }
+                }
+            }
+    
+            const data = await got(`https://api.markzynk.com/twitch/chatters/${channel}/count`, {throwHttpErrors:false}).json();
+
+            if (!data.count) {
+                return {
+                    text:'Invalid Username! oshDank', reply:true
+                };
+            }
+
+            return{text:`there are ${data.count} in ${channel}'s chat chatters`}
+
+        } catch (err) {
+            bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
+            console.log(err);
+            bot.Client.privmsg(context.channel.login, `${err.constructor.name} monkaS ${err.message}`)
+        }
+    },
+};
