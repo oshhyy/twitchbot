c4977bc0d1b7b16508d70b6a909ff6e2147c9831
oshhyy <oshfour@gmail.com>
2024-02-20 03:05:32 -0500

    asdasda

--- commands/broadcasterelo.js
+++ commands/broadcasterelo.js
@@ -46,14 +46,14 @@ module.exports = {
                 if (color) { encodedColor = encodeURIComponent(`#${color}`) }
                 return encodedColor
             }
-            function podiumColor(rank) {
-                let color
-                let encodedColor
-                if (rank == 1) { color = "EDE59A" }
-                if (rank == 2) { color = "CFCBCA" }
-                if (rank == 3) { color = "B57F6B" }
-                if (color) { encodedColor = encodeURIComponent(`#${color}`) }
-                return encodedColor
+            function podiumColor(n) {
+                let colorASD
+                let encodedColorASD
+                if (n == 1) { colorASD = "EDE59A" }
+                if (n == 2) { colorASD = "CFCBCA" }
+                if (n == 3) { colorASD = "B57F6B" }
+                if (colorASD) { encodedColorASD = encodeURIComponent(`#${color}`) }
+                return encodedColorASD
             }
             function badgeIcon(badge) {
                 if (badge == 1) { return "◇" }
@@ -73,7 +73,7 @@ module.exports = {
             const rank = mcsrData.data.eloRank ?? "?"
             const rankName = getRank(elo)
             let color
-            if (rank <= 3) {
+            if (rank < 4) {
                 color = podiumColor(rankName)
             } else { color = rankColor(rankName) }
             
