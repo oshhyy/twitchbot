a2f25f428dc16b01318d897bb6859f0fcc8d5f1b
oshhyy <oshfour@gmail.com>
2024-02-24 16:49:54 -0500

    fixed link ome

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -167,7 +167,7 @@ module.exports = {
 
             await twitchapi.changeColor(eloColor)
             await bot.Utils.sleep(1000)
-            return{text: `/me • Ranked Match Stats (${matchDate} ago) • #${p1Rank} ${p1Badge}${bot.Utils.unping(p1Player)} (${p1Elo}) VS #${p2Rank} ${p2Badge}${bot.Utils.unping(p2Player)} (${p2Elo}) • Winner: ${winner} (${finalTime}) • Elo Change: ${bot.Utils.unping(p1Player)} ${p1Change} → ${p1NewElo} | ${bot.Utils.unping(p2Player)} ${p2Change} → ${p2NewElo} • Seed Type: ${seedType} • https://mcsrrankedstats.vercel.app/oshgay/${mostRecentNonDecayMatch.id}`, reply: true}
+            return{text: `/me • Ranked Match Stats (${matchDate} ago) • #${p1Rank} ${p1Badge}${bot.Utils.unping(p1Player)} (${p1Elo}) VS #${p2Rank} ${p2Badge}${bot.Utils.unping(p2Player)} (${p2Elo}) • Winner: ${winner} (${finalTime}) • Elo Change: ${bot.Utils.unping(p1Player)} ${p1Change} → ${p1NewElo} | ${bot.Utils.unping(p2Player)} ${p2Change} → ${p2NewElo} • Seed Type: ${seedType} • https://mcsrrankedstats.vercel.app/${p1Player}/${mostRecentNonDecayMatch.id}`, reply: true}
 
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
