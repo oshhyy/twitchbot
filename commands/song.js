95f5695eb8271fcf6553a167de58f09599868667
oshhyy <oshfour@gmail.com>
2024-04-21 09:29:36 -0400

    ASDASDSD

--- commands/song.js
+++ commands/song.js
@@ -55,7 +55,7 @@ module.exports = {
             }
 
             const songData = await got(`https://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=${config.lastfmKey}&artist=${artistName}&track=${songName}&autocorrect=1&username=${lastfmName}&format=json`, {throwHttpErrors:false}).json()
-            let playCount = songData.track.playcount ?? "?"
+            let playCount = songData.track.userplaycount ?? "?"
 
                 return{text:`/me 🎵 ${artistName} • ${songName} elisVibe ${url} • play #${playCount}`, reply:true}
             
