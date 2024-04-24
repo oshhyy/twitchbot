458ea611dbafe0952e62f435ad0f4e1581e0b8a5
oshhyy <oshfour@gmail.com>
2024-04-24 19:30:34 -0400

    added bwstats

--- commands/bwstats.js
+++ commands/bwstats.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "bwstats",
     cooldown: 3000,
     aliases: ['bw'],
-    description: `bwstats [minecraft-username] | provides hypixel bedwars stats`,
+    description: `bwstats [minecraft-username] | provides hypixel bedwars stats. currently only supports overall stats, may add specific mode soon`,
     execute: async context => {
         try {
             // command code
@@ -29,15 +29,42 @@ module.exports = {
             console.log(mcUUID)
             let hypixelData;
             try {
-                hypixelData = await got(`https://api.hypixel.net/v2/player?uuid=${mcUUID}`, {headers: {'API-Key': config.hypixelKey}}, { throwHttpErrors: false }).json()
+                hypixelData = await got(`https://api.hypixel.net/v2/player?uuid=${mcUUID}`, {headers: {'API-Key': config.hypixelKey}}).json()
             } catch (err) {
-                console.log(hypixelData)
+                console.log(err)
                 return {
-                    text: `doid`, reply: true
+                    text: `Error getting hypixel data monkaS`, reply: true
                 }
             }
 
             console.log(hypixelData)
+            console.log(hypixelData.player.stats.Bedwars)
+
+            let stars = `[★${hypixelData.player.achievements.bedwars_level}]`
+
+            let rank = ""
+            if(hypixelData.player.rank || hypixelData.player.newPackageRank) {
+                rank = `[${hypixelData.player.rank ?? hypixelData.player.newPackageRank}]`
+            }
+
+            let gamesPlayed = hypixelData.player.stats.Bedwars.games_played_bedwars ?? 0;
+            let wins = hypixelData.player.stats.Bedwars.wins_bedwars ?? 0;
+            let losses = hypixelData.player.stats.Bedwars.losses_bedwars ?? 0;
+            const WinPercent = ((wins / (wins + losses)) * 100).toFixed(1);
+
+            let finalKills = hypixelData.player.stats.Bedwars.final_kills_bedwars ?? 0;
+            let finalDeaths = hypixelData.player.stats.Bedwars.final_deaths_bedwars ?? 0;
+            const fkdr = (finalKills / finalDeaths).toFixed(2);
+
+            let bedsBroken = hypixelData.player.stats.Bedwars.beds_broken_bedwars ?? 0;
+            let bedsLost = hypixelData.player.stats.Bedwars.beds_lost_bedwars ?? 0;
+            const bblr = (bedsBroken / bedsLost).toFixed(2);
+
+            let winstreak = hypixelData.player.stats.Bedwars.winstreak ?? 0
+
+            return {
+                text: `/me ${stars} ${rank} ${bot.Utils.unping(hypixelData.player.playername)} W/L ${wins}/${losses} (${WinPercent}%) • ${gamesPlayed} Games Played • FKDR: ${fkdr} (${finalKills}/${finalDeaths}) • BBLR ${bblr} (${bedsBroken}/${bedsLost}) ${winstreak} winstreak •  `, reply: true
+            }
 
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
