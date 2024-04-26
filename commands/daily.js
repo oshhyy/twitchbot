dd1a7a68e2c54813748b26d704125037135530d4
oshhyy <oshfour@gmail.com>
2024-04-26 12:27:33 -0400

    added daily weekly monthly paceman recordsdsds

---
+++ commands/daily.js
@@ -0,0 +1,46 @@
+const got = require("got");
+module.exports = {
+    name: "daily",
+    cooldown: 3000,
+    aliases: ["dailyrecord"],
+    description: `daily - shows daily PaceMan record `,
+    execute: async context => {
+        try {
+            // command code
+            function msToTime(s) {
+                // Pad to 2 or 3 digits, default is 2
+                var pad = (n, z = 2) => ('00' + n).slice(-z);
+                return pad((s % 3.6e6) / 6e4 | 0) + ':' + pad((s % 6e4) / 1000 | 0);
+            }
+
+            const currentTimeInMilliseconds = new Date().getTime();
+            let pacemanData;
+            try {
+                pacemanData = await got(`https://paceman.gg/api/cs/leaderboard?filter=0&removeDuplicates=1&date=${currentTimeInMilliseconds}`).json();
+            } catch (err) {
+                return {
+                    text: `Error getting LB data. FallCry`, reply: true
+                }
+            }
+
+            if(!pacemanData.eventList[0]) {
+                return {
+                    text: `No current daily record found! oshAlright`, reply: true
+                }
+            }
+
+            const time = msToTime(pacemanData.eventList[0].time)
+            const player = bot.Utils.unping(pacemanData.eventList[0].nickname)
+            
+            return {
+                text: `Daily PaceMan Record: ${time} by ${player}`,
+                reply: true,
+            };
+        } catch (err) {
+            console.log(err);
+            return {
+                text: `error monkaS ${err.message} `,
+            };
+        }
+    },
+};
---
+++ commands/monthly.js
@@ -0,0 +1,46 @@
+const got = require("got");
+module.exports = {
+    name: "monthly",
+    cooldown: 3000,
+    aliases: ["monthlyrecord"],
+    description: `monthly - shows monthly PaceMan record `,
+    execute: async context => {
+        try {
+            // command code
+            function msToTime(s) {
+                // Pad to 2 or 3 digits, default is 2
+                var pad = (n, z = 2) => ('00' + n).slice(-z);
+                return pad((s % 3.6e6) / 6e4 | 0) + ':' + pad((s % 6e4) / 1000 | 0);
+            }
+
+            const currentTimeInMilliseconds = new Date().getTime();
+            let pacemanData;
+            try {
+                pacemanData = await got(`https://paceman.gg/api/cs/leaderboard?filter=2&removeDuplicates=1&date=${currentTimeInMilliseconds}`).json();
+            } catch (err) {
+                return {
+                    text: `Error getting LB data. FallCry`, reply: true
+                }
+            }
+
+            if(!pacemanData.eventList[0]) {
+                return {
+                    text: `No current monthly record found! oshAlright`, reply: true
+                }
+            }
+
+            const time = msToTime(pacemanData.eventList[0].time)
+            const player = bot.Utils.unping(pacemanData.eventList[0].nickname)
+            
+            return {
+                text: `Monthly PaceMan Record: ${time} by ${player}`,
+                reply: true,
+            };
+        } catch (err) {
+            console.log(err);
+            return {
+                text: `error monkaS ${err.message} `,
+            };
+        }
+    },
+};
