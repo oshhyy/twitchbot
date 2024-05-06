ee174d2aaafa78d3e5768972f8ac2205f6856fd2
oshhyy <oshfour@gmail.com>
2024-05-06 07:05:05 -0400

    FIX OME OME OME

--- commands/nethers.js
+++ commands/nethers.js
@@ -7,53 +7,22 @@ module.exports = {
     execute: async context => {
         try {
             // command code
-            let userData, mcUUID
-            if (!context.message.args[0]) {
-                userData = await bot.db.users.findOne({ id: context.user.id })
-                mcUUID = userData?.mcid
-                if (!mcUUID) {
-                    return { text: `No MC username provided! To link your account, do '+link mc <username>'`, reply: true }
-                }
-            } else {
-                if (context.message.args[0]?.startsWith("@")) {
-                    userData = await bot.db.users.findOne({ username: context.message.args[0].replace("@", "") })
-                    mcUUID = userData?.mcid
-                    if (!mcUUID) {
-                        return { text: `This user does not have a linked mc account!`, reply: true }
-                    }
-
-                } else {
-                    let mojangData;
-                    mojangData = await got(`https://api.mojang.com/users/profiles/minecraft/${context.message.args[0]}`, { throwHttpErrors: false }).json()
-                    if (mojangData.errorMessage) {
-                        return { text: `Mojang Error: ${mojangData.errorMessage} FallCry`, reply: true }
-                    }
-                    mcUUID = mojangData.id
-                }
-            }
-
-
-            let mcNameData;
-            try {
-                mcNameData = await got(`https://sessionserver.mojang.com/session/minecraft/profile/${mcUUID}`).json();
-            } catch (err) {
-                return {
-                    text: `Error getting Name data. FallCry`, reply: true
-                }
-            }
-
-            let mcName = mcNameData.name;
+            let name = context.message.args[0]?.replace("@", "") ?? context.user.login;
 
             let netherData;
             try {
-                netherData = await got(`https://paceman.gg/stats/api/getSessionNethers/?name=${mcName}&hours=16&hoursBetween=2`).json();
+                netherData = await got(`https://paceman.gg/stats/api/getSessionStats/?name=${name}&hours=16&hoursBetween=3`).json();
             } catch (err) {
-                return {
-                    text: `This user has no submitted runs in the last 16 hours!`, reply: true
+                try{
+                    netherData = await got(`https://paceman.gg/stats/api/getSessionNethers/?name=${context.channel.login}&hours=16&hoursBetween=3`).json();
+                } catch(err) {
+                    return {
+                        text: `User ${bot.Utils.unping(name)} does not have a paceman.gg profile!`, reply: true
+                    }
                 }
             }
 
-            const count = netherData.count
+            const count = c.count
             const average = netherData.avg
             
             return {
