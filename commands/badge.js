d7b8aa3efc088db5c45636ceff4e02cb6e5ac4c6
oshhyy <oshfour@gmail.com>
2024-08-01 15:33:17 -0400

    OOPSIE POOPSIE

--- commands/badge.js
+++ commands/badge.js
@@ -37,7 +37,7 @@ module.exports = {
             }
 
             const countData = await got(`https://api.potat.app/twitch/badges?badge=${badgeID}`).json();
-            let usage = countData.data[0].badge.user_count
+            let usage = countData.data[0].user_count
 
             return {
                 text: `applied vanity badge for ${user}: ${badgeName} • ${badgeDescription} (id: ${badgeID}) • there are ${usage} users with this badge applied.`,
