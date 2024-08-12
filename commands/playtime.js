2df5fcdbf5d57c452402d9c4cad3c7f84be56620
oshhyy <oshfour@gmail.com>
2024-08-12 00:43:57 -0400

    add rsg  playtime

--- commands/playtime.js
+++ commands/playtime.js
@@ -68,19 +68,30 @@ module.exports = {
             }
 
             let mcsrData;
-            try {
-                mcsrData = await got(`https://mcsrranked.com/api/users/${mcUUID}/`).json();
-            } catch (err) {
-                return {
-                    text: `This username is not registered in MCSR Ranked! oshDank`, reply: true
-                }
+            let rankedText = ""
+            mcsrData = await got(`https://mcsrranked.com/api/users/${mcUUID}/`, { throwHttpErrors: false }).json();
+            if(mcsrData.status != "error") {
+                const playtimeTotal = humanizeNoHours(mcsrData.data.statistics.total.playtime.ranked)
+                const playtimeSeason = humanizeNoHours(mcsrData.data.statistics.season.playtime.ranked)
+                rankedText = `Ranked Playtime: ${playtimeSeason} this season, ${playtimeTotal} total`
             }
 
-            const playtimeTotal = humanizeNoHours(mcsrData.data.statistics.total.playtime.ranked)
-            const playtimeSeason = humanizeNoHours(mcsrData.data.statistics.season.playtime.ranked)
-            let badge = badgeIcon(mcsrData.data.roleType)
+            let name = context.message.args[0]?.replace("@", "") ?? context.user.login;
+            let data;
+            let rsgText = ""
+            data = await got(`https://paceman.gg/stats/api/getNPH/?name=${name}&hours=999&hoursBetween=3`, { throwHttpErrors: false }).json();
+            if(!data.error) {
+                const playtime = humanizeNoHours(data.playtime + data.walltime)
+                const resets = data.totalResets
+                rsgText = `RSG Playtime: ${playtime} w/o nethers, ${resets} resets •`
+            }
 
-            return{text: `${badge} ${bot.Utils.unping(mcsrData.data.nickname)} has played ${playtimeSeason} of ranked this season, and ${playtimeTotal} in total.`, reply: true}
+            if(rankedText == "" && rsgText == "") {
+                return{text: `This user has not played any RSG or Ranked. PoroSad`, reply: true}
+            }
+
+
+            return{text: `${bot.Utils.unping(name)} ${rsgText} ${rankedText}`, reply: true}
 
         } catch (err) {
             bot.Webhook.error(`${err.constructor.name} executing ${context.message.command} by ${context.user.login} in #${context.channel.login}`, `${context.message.text}\n\n${err}`)
