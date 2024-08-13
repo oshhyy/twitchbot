625e93ba87cdfb34f721fec677bffa8b468097a3
oshhyy <oshfour@gmail.com>
2024-08-13 04:55:11 -0400

    add boobies real

--- commands/broadcastersession.js
+++ commands/broadcastersession.js
@@ -1,26 +1,71 @@
 const got = require("got");
+const humanize = require('humanize-duration');
 module.exports = {
-    name: "broadcastersession",
+    name: "session",
     cooldown: 3000,
-    aliases: [],
+    aliases: ["sessionstats", "pacestats", "pacemanstats", "today"],
     description: `session [minecraft-username] | shows splits + average for current day`,
     execute: async context => {
         try {
             // command code
+            function humanizeNoHours(seconds) {
+                const options = {
+                    language: "shortEn",
+                    languages: {
+                        shortEn: {
+                            y: () => "y",
+                            mo: () => "mo",
+                            w: () => "w",
+                            d: () => "d",
+                            h: () => "h",
+                            m: () => "m",
+                            s: () => "s",
+                            ms: () => "ms",
+                        },
+                    },
+                    units: ["h", "m", "s"],
+                    largest: 3,
+                    round: true,
+                    conjunction: "",
+                    spacer: "",
+
+                }
+                return humanize(seconds, options);
+            }
+            function msToTime(s) {
+                // Pad to 2 or 3 digits, default is 2
+                var pad = (n, z = 2) => ('00' + n).slice(-z);
+
+                var minutes = Math.floor(s / 60000);
+                var seconds = Math.floor((s % 60000) / 1000);
+                var milliseconds = s % 1000;
+
+                return pad(minutes) + ':' + pad(seconds);
+            }
 
             let name = context.message.args[0]?.replace("@", "") ?? context.channel.login;
 
             let sessionData;
+            let nphData;
             try {
                 sessionData = await got(`https://paceman.gg/stats/api/getSessionStats/?name=${name}&hours=999&hoursBetween=3`).json();
+                nphData = await got(`https://paceman.gg/stats/api/getNPH/?name=${name}&hours=999&hoursBetween=3`, { throwHttpErrors: false }).json();
             } catch (err) {
-                return {}
+                try {
+                    name = context.channel.login;
+                    sessionData = await got(`https://paceman.gg/stats/api/getSessionStats/?name=${name}&hours=999&hoursBetween=3`).json();
+                    nphData = await got(`https://paceman.gg/stats/api/getNPH/?name=${name}&hours=999&hoursBetween=3`, { throwHttpErrors: false }).json();
+                } catch (err) {
+                    return {
+                        text: `User ${bot.Utils.unping(name)} does not have a paceman.gg profile!`, reply: true
+                    }
+                }
             }
 
             let netherText = firstStructureText = secondStructureText = firstPortalText = strongholdText = endText = finishText = "";
 
-            if(sessionData.nether.count != 0) {
-                netherText = `• nethers: ${sessionData.nether.count} (${sessionData.nether.avg} avg)`
+            if (sessionData.nether.count != 0) {
+                netherText = `• nethers: ${sessionData.nether.count} (${sessionData.nether.avg} avg, ${msToTime(nphData.avg.toFixed(0))} nph, ${nphData.rpe.toFixed(0)} rpe)`
             } else {
                 return {
                     text: `No session data found for ${bot.Utils.unping(name)}. FallCry`,
@@ -28,32 +73,32 @@ module.exports = {
                 };
             }
 
-            if(sessionData.first_structure.count != 0) {
+            if (sessionData.first_structure.count != 0) {
                 firstStructureText = `• first structures: ${sessionData.first_structure.count} (${sessionData.first_structure.avg} avg)`
             }
 
-            if(sessionData.second_structure.count != 0) {
+            if (sessionData.second_structure.count != 0) {
                 secondStructureText = `• second structures: ${sessionData.second_structure.count} (${sessionData.second_structure.avg} avg)`
             }
 
-            if(sessionData.first_portal.count != 0) {
+            if (sessionData.first_portal.count != 0) {
                 firstPortalText = `• first portals: ${sessionData.first_portal.count} (${sessionData.first_portal.avg} avg)`
             }
 
-            if(sessionData.stronghold.count != 0) {
+            if (sessionData.stronghold.count != 0) {
                 strongholdText = `• strongholds: ${sessionData.stronghold.count} (${sessionData.stronghold.avg} avg)`
             }
 
-            if(sessionData.end.count != 0) {
+            if (sessionData.end.count != 0) {
                 endText = `• end enters: ${sessionData.end.count} (${sessionData.end.avg} avg)`
-            } 
+            }
 
-            if(sessionData.finish.count != 0) {
+            if (sessionData.finish.count != 0) {
                 finishText = `• finishes: ${sessionData.finish.count} (${sessionData.finish.avg} avg)`
-            } 
-            
+            }
+
             return {
-                text: `${bot.Utils.unping(name)} Session Stats: ${netherText} ${firstStructureText} ${secondStructureText} ${firstPortalText} ${strongholdText} ${endText} ${finishText}`,
+                text: `${bot.Utils.unping(name)} Session Stats (${humanizeNoHours(nphData.playtime + nphData.walltime).replace(/,\s/g, "")}): ${netherText} ${firstStructureText} ${secondStructureText} ${firstPortalText} ${strongholdText} ${endText} ${finishText}`,
                 reply: true,
             };
         } catch (err) {
