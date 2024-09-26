5e2030c89c7c5e17fa96fcd2d30364b79c2ecb57
oshhyy <oshfour@gmail.com>
2024-09-26 11:17:24 -0400

    SSTUPID IDOOT

--- commands/joinchannel.js
+++ commands/joinchannel.js
@@ -61,8 +61,8 @@ module.exports = {
                     const channelInfo = await bot.db.channels.findOne({id: xd[0].id})
                     
                     if(channelInfo) {
-                        await bot.db.channels.updateOne( { id: xd[0].id}, { $set: { isChannel: true } } )
-                        await bot.db.channels.updateOne( { username: channel}, { $set: { username: channel } } )
+                        await bot.db.channels.updateOne( { id: xd[0].id }, { $set: { isChannel: true } } )
+                        await bot.db.channels.updateOne( { id: xd[0].id }, { $set: { username: context.message.params.channel } } )
                     } else {
                         let a = new bot.db.channels({ id: xd[0].id, username: xd[0].login, isChannel: true, joinedAt: Date.now(), settings: { offlineOnly: false }, });
                         await a.save()
