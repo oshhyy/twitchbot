b2e6c6b5fd6b0a94c8cfaa134b51ce880bb97461
oshhyy <oshfour@gmail.com>
2024-11-18 06:23:46 -0500

    fix stupid error

--- commands/nethers.js
+++ commands/nethers.js
@@ -13,7 +13,7 @@ module.exports = {
                 
                 var minutes = Math.floor(s / 60000);
                 var seconds = Math.floor((s % 60000) / 1000); 
-                // var milliseconds = s % 1000;
+                var milliseconds = s % 1000;
             
                 var formattedMinutes = minutes < 100 ? pad(minutes) : minutes;
                 return formattedMinutes + ':' + pad(seconds) + '.' + pad(milliseconds, 1);
